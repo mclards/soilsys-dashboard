e68f8eac61348fab68adbe16609cdf8b21169901
Engr. Clariden Montaño <124187893+mclards@users.noreply.github.com>
2025-11-19 19:01:20 +0800

    Final update

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow () {
       nodeIntegration: true
     }
   })
-  win.loadFile('index.html')  // your dashboard entry point
+  win.loadFile('index.html') 
 }
 
 app.whenReady().then(createWindow)
